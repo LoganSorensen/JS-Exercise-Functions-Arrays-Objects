b10b8c8c168c6285188c8a399ad6ad259b37ca27
Logan Sorensen <loganmsorensen@gmail.com>
2019-11-11 15:38:02 -0800

    actually completed the 2nd challenge

--- index.js
+++ index.js
@@ -64,9 +64,13 @@ console.log(makePersonObject);
  * the returned value should look like `Hello, my name is Leia`.
 */
 function getName(name) {
-  return "Hello, my name is ${name}";
+  const nameObj = {
+    name: "Luke"
+  }
+  return "Hello, my name is " + nameObj.name;
 }
-getName("Luke")
+
+
 
 /**
  * ### Challenge `makeSmartPerson`
@@ -81,8 +85,17 @@ getName("Luke")
  *         and returns a string like `Hello, my name is {name}`.
  *         where `{name}` is the name passed into `makeSmartPerson`.
 */
-function makeSmartPerson(/* code here */) {
-  /* code here */
+function makeSmartPerson(name) {
+  const smartObj ={
+    name: name,
+    sum: function(num1, num2){
+      return num1 + num2;
+    },
+    speak: function(){
+      return `Hello my name is ${name}`;
+    }
+  }
+  // smartObj("Luke");
 }
 
 
