05a266f9e6347801d7b32be38ad191e119961c9a
Logan Sorensen <loganmsorensen@gmail.com>
2019-11-11 13:10:27 -0800

    completed 2nd challenge

--- index.js
+++ index.js
@@ -63,9 +63,10 @@ console.log(makePersonObject);
  * passing { id: 1, name: 'Leia', email: 'leia@leia.com` } as the argument,
  * the returned value should look like `Hello, my name is Leia`.
 */
-function getName(/* code here */) {
-  /* code here */
+function getName(name) {
+  return "Hello, my name is ${name}";
 }
+getName("Luke")
 
 /**
  * ### Challenge `makeSmartPerson`
