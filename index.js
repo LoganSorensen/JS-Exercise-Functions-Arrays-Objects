8a967e79d981fac1b87ddbaa4d6125023db5b904
Logan Sorensen <loganmsorensen@gmail.com>
2019-11-16 14:48:56 -0800

    passed getModelYears

--- index.js
+++ index.js
@@ -216,8 +216,13 @@ function sortCarInventory(inventory) {
  *     (1) an array which is an inventory of cars like the one inside /data/inventory.js.
  * getModelYears returns an array containing all the 'car_year's in the inventory.
 */
-function getModelYears(/* code here */) {
-  /* code here */
+function getModelYears(inventory) {
+  const yearsArr = [];
+  for (let i = 0; i < inventory.length; i++){
+    yearsArr[i] = inventory[i].car_year;
+  }
+  
+  return yearsArr;
 }
 
 /**
