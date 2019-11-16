b7e73d1abcdea0c58bc1520a4e123f45eeb21e53
Logan Sorensen <loganmsorensen@gmail.com>
2019-11-16 14:21:36 -0800

    passed makeSmartPerson

--- index.js
+++ index.js
@@ -61,8 +61,11 @@ function makePersonObject(id, name, email) {
  * passing { id: 1, name: 'Leia', email: 'leia@leia.com` } as the argument,
  * the returned value should look like `Hello, my name is Leia`.
 */
-function getName(/* code here */) {
-  /* code here */
+function getName(name) {
+  const nameObj = {
+    name: name
+  }
+  return `Hello, my name is ${nameObj.name}`;
 }
 
 /**
@@ -78,8 +81,17 @@ function getName(/* code here */) {
  *         and returns a string like `Hello, my name is {name}`.
  *         where `{name}` is the name passed into `makeSmartPerson`.
 */
-function makeSmartPerson(/* code here */) {
-  /* code here */
+function makeSmartPerson(name) {
+  const smartPersonObj = {
+    name: name,
+    sum: function(num1, num2){
+      return num1 + num2;
+    },
+    speak: function(){
+      return `Hello, my name is ${name}`;
+    }
+  }
+  return smartPersonObj;
 }
 
 
