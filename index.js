84d0d343d61953613af64e83b05843acd53d7f72
Logan Sorensen <loganmsorensen@gmail.com>
2019-11-16 14:28:05 -0800

    passed getLastCarInfo

--- index.js
+++ index.js
@@ -167,8 +167,9 @@ function getCarInfoByIndex(inventory, index) {
  * For example, if getLastCarInfo is invoked passing the inventory inside /data/inventory.js,
  * it will return `This is a Lincoln Town Car`.
 */
-function getLastCarInfo(/* code here */) {
-  /* code here */
+function getLastCarInfo(inventory) {
+  const last = inventory[inventory.length - 1];
+  return `This is a ${last.car_make} ${last.car_model}`;
 }
 
 /**
